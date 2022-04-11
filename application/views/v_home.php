94ee90a6f41b2ae02403d7b147251c6f9ae630fa
Anang20 <anangsyah766@gmail.com>
2022-04-11 16:06:51 +0700

    update color

--- application/views/v_home.php
+++ application/views/v_home.php
@@ -18,11 +18,11 @@
 
 <!-- Earnings (Monthly) Card Example -->
 <div class="col-xl-3 col-md-6 mb-4">
-    <div class="card border-left-success shadow h-100 py-2">
+    <div class="card border-left-danger shadow h-100 py-2">
         <div class="card-body">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
                         Earnings (Annual)</div>
                     <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
                 </div>
