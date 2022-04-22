03a0c8752be1587e7e2464ef0f64dd2583aeebe7
Anang20 <anangsyah766@gmail.com>
2022-04-22 08:50:08 +0700

    update menu dashboard

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -9,20 +9,15 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>E-Perpus</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
-
     <!-- Custom styles for this page -->
     <link href="<?= base_url('assets/'); ?>vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-
 
 </head>
 
@@ -156,25 +151,18 @@
 
     <!-- Core plugin JavaScript-->
     <script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
-
     <!-- Custom scripts for all pages-->
     <script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
-
     <!-- Page level plugins -->
     <script src="<?= base_url('assets/'); ?>vendor/chart.js/Chart.min.js"></script>
-
     <!-- Page level custom scripts -->
     <script src="<?= base_url('assets/'); ?>js/demo/chart-area-demo.js"></script>
     <script src="<?= base_url('assets/'); ?>js/demo/chart-pie-demo.js"></script>
-
     <!-- Page level plugins -->
     <script src="<?= base_url('assets/'); ?>vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="<?= base_url('assets/'); ?>vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
      <!-- data tables -->
      <script src="<?= base_url('assets/'); ?>js/demo/datatables-demo.js"></script>
-
-
 </body>
 
 </html>
--- application/views/v_menu.php
+++ application/views/v_menu.php
@@ -29,21 +29,13 @@
         </a>
     </li>
 
-    <!-- Nav Item - Data Master -->
+    <!-- Nav Item - Data Buku  -->
     <li class="nav-item active">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-desktop"></i>
-            <span>Master Buku</span>
+        <!-- akan di arahkan ke controller anggota -->
+        <a class="nav-link" href="<?= base_url('Buku'); ?>">
+            <i class="fas fa-book"></i>
+            <span>Data Buku</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Master Buku</h6>
-                <a class="collapse-item" href="#">Pengarang</a>
-                <a class="collapse-item" href="#">Penerbit</a>
-                <a class="collapse-item" href="#">Buku</a>
-            </div>
-        </div>
     </li>
 
     <!-- Divider -->
