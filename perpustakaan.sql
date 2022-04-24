24e2c9cd5338bcccd13e41f8898be942fc579689
Anang20 <anangsyah766@gmail.com>
2022-04-24 11:33:58 +0700

    update database tabel peminjaman & pengembalian

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 22 Apr 2022 pada 03.43
+-- Waktu pembuatan: 24 Apr 2022 pada 06.31
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -40,10 +40,17 @@ CREATE TABLE `anggota` (
 --
 
 INSERT INTO `anggota` (`id_anggota`, `nama_anggota`, `jenis_kelamin`, `alamat`, `no_hp`) VALUES
-(1, 'Anang Syah ', 'Laki-laki', 'Kancilan RT3 RW1', '089670068639'),
 (2, 'Reza Nur', 'Laki-laki', 'krasak', '085xxxxxxxxxxx'),
 (3, 'Zaninuddin', 'Laki-laki', 'Wedelan', '082xxxxxxxxxxxx'),
-(4, 'havis', 'Laki-laki', 'kembang', '08xxxxxxxxxxx');
+(12, 'yosafat', 'Laki-laki', 'tubanan', '082xxxxxxxxxxxx'),
+(13, 'Anang Syah Amirul Haqim', 'Laki-laki', 'Kancilan', '089670068639'),
+(14, 'putri', 'Perempuan', 'jepara', '08xxxxxxxxxxx'),
+(15, 'syifa', 'Perempuan', 'semarang', '085xxxxxxxxx'),
+(16, 'Ferdi', 'Laki-laki', 'Jepara', '082xxxxxxxxxxxx'),
+(17, 'Reza Ap', 'Laki-laki', 'Bangsri', '089xxxxxxxxxxxx'),
+(18, 'Jamal', 'Laki-laki', 'Bogor', '085xxxxxxxxx'),
+(19, 'Dinda', 'Perempuan', 'Jakarta', '085xxxxxxxxx'),
+(20, 'Zoro', 'Laki-laki', 'Jogjakarta', '08xxxxxxxxxxx');
 
 -- --------------------------------------------------------
 
@@ -65,8 +72,11 @@ CREATE TABLE `buku` (
 --
 
 INSERT INTO `buku` (`id_buku`, `pengarang`, `penerbit`, `judul_buku`, `tahun_terbit`, `jumlah`) VALUES
-(4, 'stefano', 'agus kotak', 'Belajar React  native', 2018, 10),
-(5, 'Anang', 'zorojuro', 'Belajar Laravel', 2022, 5);
+(8, 'stefano', 'agus kotak', 'Belajar Laravel', 2016, -1),
+(9, 'Anang', 'bagus', 'Belajar React JS', 2017, 9),
+(10, 'Wahyu', 'Wahyu', 'Pemrograman Android', 2020, 5),
+(11, 'Brando', 'Brandon', 'Fundamental API', 2022, 19),
+(12, 'ahmad', 'ahmad', 'Pemrograman Javascript', 2022, 28);
 
 -- --------------------------------------------------------
 
@@ -88,6 +98,63 @@ CREATE TABLE `login` (
 
 INSERT INTO `login` (`id`, `nama`, `username`, `password`, `level`) VALUES
 (1, 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_pinjam` int(11) NOT NULL,
+  `id_anggota` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_pinjam`, `id_anggota`, `id_buku`, `tgl_pinjam`, `tgl_kembali`) VALUES
+(1, 13, 9, '2022-04-12', '2022-04-19'),
+(5, 15, 11, '2022-04-22', '2022-04-29'),
+(7, 16, 8, '2022-04-23', '2022-04-30'),
+(9, 2, 12, '2022-04-23', '2022-04-30'),
+(10, 18, 13, '2022-04-23', '2022-04-30');
+
+--
+-- Trigger `peminjaman`
+--
+DELIMITER $$
+CREATE TRIGGER `jmlh_after_pinjam` AFTER INSERT ON `peminjaman` FOR EACH ROW UPDATE buku SET buku.jumlah = buku.jumlah -1 WHERE buku.id_buku = new.id_buku
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pengembalian`
+--
+
+CREATE TABLE `pengembalian` (
+  `id_pengembalian` int(11) NOT NULL,
+  `id_anggota` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `tgl_pinjam` date NOT NULL,
+  `tgl_kembali` date NOT NULL,
+  `tgl_kembalikan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pengembalian`
+--
+
+INSERT INTO `pengembalian` (`id_pengembalian`, `id_anggota`, `id_buku`, `tgl_pinjam`, `tgl_kembali`, `tgl_kembalikan`) VALUES
+(1, 14, 10, '2022-04-22', '2022-04-29', '2022-04-23'),
+(2, 17, 12, '2022-04-23', '2022-04-30', '2022-04-23'),
+(3, 16, 10, '2022-04-23', '2022-04-30', '2022-04-23');
 
 --
 -- Indexes for dumped tables
@@ -112,6 +179,18 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_pinjam`);
+
+--
+-- Indeks untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  ADD PRIMARY KEY (`id_pengembalian`);
+
+--
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -119,19 +198,31 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT untuk tabel `anggota`
 --
 ALTER TABLE `anggota`
-  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT untuk tabel `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `login`
 --
 ALTER TABLE `login`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_pinjam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `pengembalian`
+--
+ALTER TABLE `pengembalian`
+  MODIFY `id_pengembalian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
