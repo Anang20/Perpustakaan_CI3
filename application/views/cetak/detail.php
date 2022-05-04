72a16816c840c968609cb221e487570f07792ac9
zainuddin25 <zanukjepara@gmail.com>
2022-05-04 21:14:04 +0700

    fitur cetak kartu anggota

---
+++ application/views/cetak/detail.php
@@ -0,0 +1,56 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 mb-3">
+            <a href="<?= base_url('cetak'); ?>" class="btn btn-sm btn-secondary shadow-sm"><i class="fas fa-id-card"></i> Tabel Anggota</a>
+            <a href="<?= base_url('cetak'); ?>" class="btn btn-sm btn-primary shadow-sm"><i class="fas fa-id-card"></i> Download Kartu</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-dark">
+                <div class="row">
+                    <div class="col-8 mx-auto border rounded">
+                        <div class="row">
+                            <div class="col-2">
+                                <img src="<?= base_url('assets/'); ?>img/about-img.jpg" class="rounded mx-auto d-block m-2" alt="logo" style="width: 100%;">
+                            </div>
+                            <div class="col-10 text-center mt-4">
+                                <h5><b>Kartu Anggota E-Perpus</b></h5>
+                                <small>Sebagai tanda anggota perpustakaan yang digunakan untuk meminjam buku</small>
+                            </div>
+                        </div>
+                        <hr class="mt-1">
+                        <div class="row">
+                            <div class="col-3">
+                                <img src="<?= base_url('assets/'); ?>img/about-img.jpg" class="rounded mx-auto d-block m-2" alt="logo" style="width: 100%;">
+                            </div>
+                            <div class="col-9 mt-1">
+                                <!-- Nama Anggota -->
+                                <p>Nama Anggota : <?= $data["nama_anggota"] ?> </p>
+                                <p>Alamat : <?= $data["alamat"] ?></p>
+                                <p>Jenis Kelamin : <?= $data["jenis_kelamin"] ?></p>
+                                <p>Nomor Handphone : <?= $data["no_hp"] ?></p>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-8 mt-1 mb-1">
+                                <p class="">Kode Anggota : <b><?= $data["kode_anggota"] ?></b></b></p>
+                            </div>
+                            <div class="col-4">
+                                <span>
+                                    <?php 
+                                        $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+                                        echo $generator->getBarcode($data["kode_anggota"], $generator::TYPE_CODE_128);
+                                    ?>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
---
+++ assets/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a3c0690a5a9510ec6e7897204760be3a3e679edc',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a3c0690a5a9510ec6e7897204760be3a3e679edc',
+            'dev_requirement' => false,
+        ),
+        'picqer/php-barcode-generator' => array(
+            'pretty_version' => 'v2.2.1',
+            'version' => '2.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../picqer/php-barcode-generator',
+            'aliases' => array(),
+            'reference' => '49022793ff09569c57a2621ba58a191db5a421c4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
